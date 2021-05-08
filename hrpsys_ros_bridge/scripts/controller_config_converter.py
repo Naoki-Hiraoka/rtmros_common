7d7d4084d3f8be83f0ecaebef880aa8182aa61c4
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-05-09 03:34:14 +0900

    [hrpsys_ros_bridge] use limb-list in robot-model and limbs param in yaml

--- hrpsys_ros_bridge/scripts/controller_config_converter.py
+++ hrpsys_ros_bridge/scripts/controller_config_converter.py
@@ -32,7 +32,9 @@ if __name__ == '__main__':
 
     lst = yaml.load(open(inputfile).read())
 
-    limb_candidates = ('larm', 'rarm', 'lleg', 'rleg', 'torso', 'head', 'rhand', 'lhand') ## candidates of limb names
+    limb_candidates = ('larm', 'rarm', 'lleg', 'rleg', 'torso', 'head') ## candidates of limb names
+    if "limbs" in lst.keys():
+        limb_candidates = lst["limbs"]
 
     invalid_yaml = True
     for l in lst.keys():
